3767f72aff98336626965c7c9bd1ed45fcc7ee9d
yodalama <yodalama@users.noreply.github.com>
2018-07-25 14:18:14 -0700

    Continuing changes for 5-4

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = express()
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
 var messages = [
     {name: 'Tim', message: 'Good_day'},
@@ -15,7 +16,6 @@ app.get('/messages', (req, res) => {
 })
 
 app.post('/messages', (req, res) => {
-    console.log(req.body)
     messages.push(req.body)
     res.sendStatus(200)
 })
