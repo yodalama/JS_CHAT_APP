3a03571272e8157f751bc3fe566ae59946541122
yodalama <yodalama@users.noreply.github.com>
2018-07-25 09:23:13 -0700

    Get and messages updates

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ var express = require('express')
 var app = express()
 
 app.use(express.static(__dirname))
+
+var messages = [
+    {name: 'Tim', message 'Good day!'},
+    {name: 'Jane', message 'Greetings'}
+]
+
+app.get('/messages', (req, res) => {
+    res.send(messages)
+})
+
+
 var server = app.listen(3000, () => {
     console.log('Server is listening on port', server.address().port)
 })
