be47eea5ab80e56b3c14ece67ad9511242bd7d65
yodalama <yodalama@users.noreply.github.com>
2018-07-25 14:14:42 -0700

    Jave node lesson 5-4

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
 
 var messages = [
-    {name: 'Tim', message 'Good day!'},
-    {name: 'Jane', message 'Greetings'}
+    {name: 'Tim', message: 'Good_day'},
+    {name: 'Jane', message: 'Greetings'}
 ]
 
 app.get('/messages', (req, res) => {
     res.send(messages)
 })
 
+app.post('/messages', (req, res) => {
+    console.log(req.body)
+    messages.push(req.body)
+    res.sendStatus(200)
+})
 
 var server = app.listen(3000, () => {
     console.log('Server is listening on port', server.address().port)
