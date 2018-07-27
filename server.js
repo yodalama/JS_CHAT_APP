cafe2a133ea27af579ed961d86af0286564604d5
yodalama <yodalama@users.noreply.github.com>
2018-07-27 10:03:07 -0700

    continuing with setup of socket.io 5-5

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
