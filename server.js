5307f0b222195c83e42080ca7b381e1b5bd5a06d
yodalama <yodalama@users.noreply.github.com>
2018-07-27 10:29:08 -0700

    finished with socket.io 5-6

--- server.js
+++ server.js
@@ -19,9 +19,14 @@ app.get('/messages', (req, res) => {
 
 app.post('/messages', (req, res) => {
     messages.push(req.body)
+    io.emit('message', req.body)
     res.sendStatus(200)
 })
 
-var server = app.listen(3000, () => {
+io.on('connection', (socket) => {
+    console.log('a user connected')
+})
+
+var server = http.listen(3000, () => {
     console.log('Server is listening on port', server.address().port)
 })
